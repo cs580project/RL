17565873b5450fdebe5d615080af1e957e7ce811
mriches <mriches@gmail.com>
2014-07-05 23:33:22 -0700

    Start adding code for RLGame class

--- cs580RL/Source/RLGame.h
+++ cs580RL/Source/RLGame.h
@@ -2,6 +2,7 @@
 #include "statemch.h"
 #include "RLWorld.h"
 #include "RLearner.h"
+
 class RLGame : public StateMachine
 {
 
@@ -13,9 +14,16 @@ public:
 	void init();
 
 private:
-	RLearner rLearner;
-	RLWorld theWorld;
+    virtual bool States(State_Machine_Event event, MSG_Object * msg, int state, int substate);
 
-	virtual bool States(State_Machine_Event event, MSG_Object * msg, int state, int substate);
+	RLearner        m_RLearner;
+	RLWorld         m_learningWorld;
+
+    float           m_punishmentValue;
+    float           m_rewardValue;
+    LearningMethod  m_learningMethod;
+    int             m_trainingIterations;
+    int             m_iterationsPerFrame;
+
 };
 
