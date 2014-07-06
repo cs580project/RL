114e323da67c8e108fd2f4ec697a03ec2c2767a1
watson9144 <watson9144@gmail.com>
2014-07-06 01:42:22 -0700

    Add something in RLWorld

--- cs580RL/Source/RLWorld.cpp
+++ cs580RL/Source/RLWorld.cpp
@@ -1,43 +1,242 @@
 #include "DXUT.h"
 #include "RLWorld.h"
+#include "terrain.h"
+#include "global.h"
 
 
 RLWorld::RLWorld()
 {
+	InitialValue();
+	loadMapInfo();
+	resetState();
 }
 
 
 RLWorld::~RLWorld()
 {
+	for (int i = 0; i < mapWidth; i++)
+		delete currentMap[i];
+
+	delete[] currentMap;
 }
 
 float RLWorld::getReward()
 {
-	return 0;
+	return waitingReward;
 }
 
-
-vector<int>& RLWorld::getNextState(int action)
-{
-	return vector<int>();
-}
 
 vector<int>& RLWorld::getCurrentState()
 {
-	return vector<int>();
+	stateArray[0] = mx;
+	stateArray[1] = my;
+	stateArray[2] = cx;
+	stateArray[3] = cy;
+	stateArray[4] = chx;
+	stateArray[5] = chy;
+	
+	return stateArray;
+}
+
+vector<int>& RLWorld::getNextState(int action)
+{
+	// action is mouse action:  0=u 1=ur 2=r 3=dr ... 7=ul
+	Pos nPos = getCoords(action);
+	
+	if (!isWall(nPos.x, nPos.y)) {
+		// move agent
+		mx = nPos.x; 
+		my = nPos.y;
+	}
+	else 
+	{
+		//illegal action, do something
+	}
+	// update world
+	moveCat();
+	waitingReward = calcReward();
+
+	// if mouse has cheese, relocate cheese
+	/*if ((mx == chx) && (my == chy)) {
+		d = getRandomPos();
+		chx = d.width;
+		chy = d.height;
+	}*/
+
+	/*// if cat has mouse, relocate mouse
+	if ((mx==cx) && (my==cy)) {
+	d = getRandomPos();
+	mx = d.width;
+	my = d.height;
+	}*/
+
+	return getCurrentState();
 }
 
 void RLWorld::resetState()
 {
-
+	catScores = 0;
+	mouseScores = 0;
+	waitingReward = calcReward();
+	setRandomPos();
 }
 
-bool RLWorld::validAction()
+bool RLWorld::validAction(int action)
 {
-	return 0;
+	Pos nPos = getCoords(action);
+	return !isWall(nPos.x, nPos.y);
 }
 
 bool RLWorld::endState()
 {
-	return 0;
+	return ((cx == mx) && (cy == my));
+}
+
+double RLWorld::calcReward()
+{
+	double newReward = 0;
+	if ((mx == chx) && (my == chy)) 
+	{
+		mouseScores++;
+		newReward += currentReward;
+	}
+
+	if ((cx == mx) && (cy == my)) 
+	{
+		catScores++;
+		newReward -= currentPunish;
+	}
+
+	return newReward;
+}
+
+bool RLWorld::isWall(int a, int b)
+{
+	if (a < 0 || b < 0 || a >= mapWidth || b >= mapWidth) return true;
+	
+	bool returnVal = g_terrain.IsWall(a,b);
+
+	return returnVal;
+}
+
+Pos RLWorld::getRandomPos()
+{
+	
+	Pos nPos;
+	
+	while (true)
+	{ 
+		nPos.x = (int)(rand() % mapWidth);
+		nPos.y = (int)(rand() % mapWidth);
+	
+		if (!isWall(nPos.x, nPos.y)) return nPos;
+	}
+}
+
+void RLWorld::moveCat()
+{
+	Pos nPos;
+	nPos = moveToNewPos(cx, cy, mx, my);
+	cx = nPos.x;
+	cy = nPos.y;
+}
+
+void RLWorld::moveMouse()
+{
+	Pos nPos;
+	nPos = moveToNewPos(mx, my, chx, chy);
+	mx = nPos.x;
+	my = nPos.y;
+}
+
+Pos RLWorld::moveToNewPos(int currentx, int currenty, int targetx, int targety)
+{
+	Pos nPos;
+	
+	if (targetx == currentx) nPos.x = currentx;
+	else nPos.x += (targetx - currentx) / abs(targetx - currentx); // +/- 1 or 0
+	if (targety == currenty) nPos.y = currenty;
+	else nPos.y += (targety - currenty) / abs(targety - currenty); // +/- 1 or 0
+
+	// check if move legal	
+	if(!isWall(nPos.x,nPos.y)) return nPos;
+
+	// not legal, make random move
+	while (true) 
+	{
+		nPos.x = currentx;
+		nPos.y = currenty;
+
+		nPos.x += 1 - (int)(rand() % 3);		//get a random position then
+		nPos.y += 1 - (int)(rand() % 3);
+
+		if (!isWall(nPos.x, nPos.y)) return nPos;
+	}
+}
+
+void RLWorld::loadMapInfo()
+{
+	mapWidth = g_terrain.GetWidth();
+
+	currentMap = new int*[mapWidth];	
+
+	for (int i = 0; i < mapWidth; i++)
+		currentMap[i] = new int[mapWidth];
+}
+
+void RLWorld::setRandomPos()
+{
+	Pos nPos = getRandomPos();
+	cx = nPos.x;
+	cy = nPos.y;
+
+	while (true)
+	{
+		nPos = getRandomPos();
+		if (nPos.x != cx || nPos.y != cy) break;
+	}
+
+	mx = nPos.x;
+	my = nPos.y;
+
+	while (true)
+	{
+		nPos = getRandomPos();
+		if ((nPos.x != cx || nPos.y != cy)&&(nPos.x!=mx||nPos.y!=my)) break;
+	}
+	
+	chx = nPos.x;
+	chy = nPos.y;
+}
+
+void RLWorld::InitialValue()
+{
+	vector<int> stateArray;
+
+	for (int i = 0; i < 6; i++)
+		stateArray.push_back(-1);
+
+	currentReward = 0;
+	currentPunish = 0;
+}
+
+Pos RLWorld::getCoords(int action)
+{
+	Pos nPos;
+	nPos.x = mx;
+	nPos.y = my;
+
+	switch (action) {
+	case 0: nPos.y = my - 1; break;							//go S
+	case 1: nPos.y = my - 1; nPos.x = mx + 1; break;		//go SE
+	case 2: nPos.x = mx + 1; break;							//go E
+	case 3: nPos.y = my + 1; nPos.x = mx + 1; break;		//go NE
+	case 4: nPos.y = my + 1; break;							//go N
+	case 5: nPos.y = my + 1; nPos.x = mx - 1; break;		//go NW
+	case 6: nPos.x = mx - 1; break;							//go W
+	case 7: nPos.y = my - 1; nPos.x = mx - 1; break;		//go SW
+	default: 
+		break;
+	}
+	return nPos;
 }
--- cs580RL/Source/RLWorld.h
+++ cs580RL/Source/RLWorld.h
@@ -1,19 +1,53 @@
 #pragma once
 #include <vector>
 using namespace std;
+
+struct Pos
+{int x, y;};
+
 class RLWorld
 {
 public:
 	RLWorld();
 	~RLWorld();
 
+	vector<int> stateArray; //include coords for mouse,cat,cheese
+
 	float getReward();
 	vector<int>& getNextState(int action);
 	vector<int>& getCurrentState();
 	void resetState();
-	bool validAction();
+	bool validAction(int action);	//check if the action is legal
 	bool endState();
+
+	void setRewardVal(float reward) { currentReward = reward; }
+	void setPunishVal(float punish) { currentPunish = punish; }
+
 private:
-	vector<int> currentState;
+	vector<int> currentState;	
+	int mx, my;	//coordinates for mouse
+	int cx, cy; //coord for cat
+	int chx, chy;	// coord for cheese
+	int catScores, mouseScores;	//keep the score
+	double waitingReward;
+	float currentReward, currentPunish;
+	int** currentMap;	//map info
+	int mapWidth;
+
+	void loadMapInfo();
+	double calcReward();	//calculate reward(waiting reward)
+
+	bool isWall(int a, int b);
+	
+	Pos getRandomPos();
+	void setRandomPos();
+
+	void moveMouse();	//no learning
+	void moveCat();
+
+	void InitialValue();
+	Pos moveToNewPos(int currentx, int currenty, int targetx, int targety);
+
+	Pos getCoords(int action);
 };
 
