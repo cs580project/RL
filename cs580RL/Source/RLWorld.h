3071f1176d505541297bb952b67e69fde148d3dd
watson9144 <watson9144@gmail.com>
2014-07-06 13:59:55 -0700

    add RLWorld to master

--- cs580RL/Source/RLWorld.h
+++ cs580RL/Source/RLWorld.h
@@ -1,19 +1,55 @@
 #pragma once
 #include <vector>
 using namespace std;
+
+struct Pos
+{
+	int x, y;
+};
+
 class RLWorld
 {
 public:
 	RLWorld();
 	~RLWorld();
 
+	vector<int> stateArray;
+
 	float getReward();
 	vector<int>& getNextState(int action);
 	vector<int>& getCurrentState();
 	void resetState();
-	bool validAction();
+	bool validAction(int action);	//check if the action is legal
 	bool endState();
+
+	void setRewardVal(float reward) { currentReward = reward; }
+	void setPunishVal(float punish) { currentPunish = punish; }
+
 private:
-	vector<int> currentState;
+	vector<int> currentState;	//include coords for mouse,cat,cheese
+	int mx, my;	//coordinates for mouse
+	int cx, cy; //coord for cat
+	int chx, chy;	// coord for cheese
+	int catScores, mouseScores;	//keep the score
+	double waitingReward;
+	float currentReward, currentPunish;
+	int** currentMap;	//map info
+	int mapWidth;
+
+	void loadMapInfo();
+	double calcReward();	//calculate reward(waiting reward)
+
+	bool isWall(int a, int b);
+
+	Pos getRandomPos();
+	void setRandomPos();
+
+	void moveMouse();	//no learning
+	void moveCat();
+
+	void InitialValue();
+	Pos moveToNewPos(int currentx, int currenty, int targetx, int targety);
+
+	Pos getCoords(int action);
 };
 
